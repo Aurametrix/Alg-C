3b7076bb068ecf80f6e0b79c16c2bc5584aa2dd4
Irene <irene@aurametrix.com>
2013-09-10 16:58:39 -0700

    header file

---
+++ HASH/Money.cpp
@@ -0,0 +1,29 @@
+#include "money.h"
+#include <string>
+using namespace std;
+
+        Money::Money(int d, int c)
+        {
+           pennies = d * 100 + c;
+        }
+        Money::Money(int d)
+        {
+           pennies = d * 100;
+        }
+        int Money::Dollars() const { return pennies / 100; }
+        int Money::Cents()   const { return pennies % 100; }
+
+        void Money::Print()  const
+        {
+           cout << "$" << Dollars() << ".";
+           if ( Cents() < 10 ) cout << "0";
+           cout << Cents();
+        }
+        void Money::Add(const Money &other)
+        {
+           pennies += other.pennies;
+        }
+        void Money::AddInterest(float rate)
+        {
+           pennies += int(pennies * rate + 0.5);
+        }
