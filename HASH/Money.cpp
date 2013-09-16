db0a3b694ba5ec376b534aef3124c4b6fa62a2fb
Irene <irene@aurametrix.com>
2013-09-16 14:51:55 -0700

    better working

--- HASH/Money.cpp
+++ HASH/Money.cpp
@@ -3,28 +3,51 @@
 
 using namespace std;
 
-        Money::Money(int d, int c)
-        {
-           pennies = d * 100 + c;
-        }
-        Money::Money(int d)
-        {
-           pennies = d * 100;
-        }
-        int Money::Dollars() const { return pennies / 100; }
-        int Money::Cents()   const { return pennies % 100; }
-
-        void Money::Print()  const
-        {
-           cout << "$" << Dollars() << ".";
-           if ( Cents() < 10 ) cout << "0";
-           cout << Cents();
-        }
-        void Money::Add(const Money &other)
-        {
-           pennies += other.pennies;
-        }
-        void Money::AddInterest(float rate)
-        {
-           pennies += int(pennies * rate + 0.5);
-        }
+int main()
+{
+	// Common convention is that constants are all capital
+	const int PENNY = 1;
+	const int NICKEL = 5;
+	const int DIME = 10;
+	const int QUARTER = 25;
+	
+	int money;
+	int coin;
+	
+	cout << "Please enter in a dollar amount to convert it to change ";
+	cin >> money;
+	
+	cout << "Do you want to convert it to: \n"
+	<< "1) Pennies\n"
+	<< "2) Nickels\n"
+	<< "3) Dimes\n"
+	<< "4) Quarters" << endl;
+	
+	char choice;
+	cin >> choice;
+	
+	switch(choice)
+	{
+		case '1':
+			// Do Pennies Here
+			coin = PENNY;
+			break;
+		case '2':
+			// Do Nickels Here
+			coin = NICKEL;
+			break;
+		case '3':
+			// Do Dimes Here
+			coin = DIME;
+			break;
+		case '4':
+			// Do Quarters Here
+			coin = QUARTER;
+			break;
+		default:
+			// If the user entered an invalid choice handle it here.
+			return -1;
+	}
+	cout << "The result is: " << ((money * 100) / coin) << " in " << money << " dollars." << endl;
+	return 0;
+}
