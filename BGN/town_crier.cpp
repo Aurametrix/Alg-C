baf311429ac7f1143904c5b1b935e18fde33154c
Irene <irene@aurametrix.com>
2014-08-09 21:29:54 -0700

    storing and displaying strings

---
+++ BGN/town_crier.cpp
@@ -0,0 +1,19 @@
+//1.2 - Town Crier Program - Dirk Henkemans
+#include<iostream>
+#include<string>
+
+
+int main(void)
+{
+    using std:: count
+    using std:: string
+
+    string yell = "A dragon is coming, take cover!!!\n";
+
+    cout<< yell.c_str() << "\n"
+        << yell.c_str() << "\n"
+        << yell.c_str() << "\n"
+        << yell.c_str() << "\n";
+
+    return 0;
+}
