26f8aa4ced8c39ea087a70e1116bd5f84c42bfea
Aurametrix <irene@aurametrix.com>
2016-03-28 14:57:13 -0700

    switch  -  faster than equivalent if-else-if
    Unlike C++, C# does not allow execution to continue from one switch section to the next

---
+++ BGN/basics.cs
@@ -0,0 +1,18 @@
+// test http://csharppad.com
+
+int myInt = 1;
+switch(myInt)
+ {
+      case 1:
+         Console.Write("Hello");
+         break;
+      case 2:
+         Console.Write("John");
+         break;
+      case 3:
+         Console.Write("Goodbuy");
+         break;
+      default:
+         Console.Write("NiceDay");
+         break;
+}
