4ca896f80524ad2f5ba272c2e1fafe1481f25b23
Aurametrix <irene@aurametrix.com>
2016-03-28 14:49:30 -0700

    left-shift and right shift operators

---
+++ BGN/shift.cs
@@ -0,0 +1,27 @@
+class MainClass8
+{
+    static void Main()
+    {
+        int a = 1000;
+        a >>= 4;
+        Console.WriteLine(a);
+    }
+}
+/*
+Output:
+62
+*/
+
+class MainClass9
+{
+    static void Main()
+    {
+        int a = 1000;
+        a <<= 4;
+        Console.WriteLine(a);
+    }
+}
+/*
+Output:
+16000
+*/
