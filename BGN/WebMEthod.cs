44c81520c1abe547b3850ece283ac00dbe6af6e3
Aurametrix <irene@aurametrix.com>
2016-04-03 18:11:05 -0700

    WebMethod attribute, method exposed as part of the XML Web service

---
+++ BGN/WebMEthod.cs
@@ -0,0 +1,8 @@
+public class Service1 : System.Web.Services.WebService
+{ 
+    [System.Web.Services.WebMethod(BufferResponse=false)]
+    public DataSet GetBigData()
+    {
+       //implementation code
+    }
+}
