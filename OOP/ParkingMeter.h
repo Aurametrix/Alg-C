07f80855887a1b983715b6feebc8b9adaa20181f
Aurametrix <irene@aurametrix.com>
2017-03-18 17:43:05 -0700

    The class's only responsibility is to know the number of minutes of parking time that has been purchased.

---
+++ OOP/ParkingMeter.h
@@ -0,0 +1,11 @@
+/**
+ * This class should simulate a parking ticket, see exercise for description
+ * of responsibilities
+ *
+ */
+class ParkingMeter {
+
+    private int minutesPurchased;
+
+    //omitted getter/setters
+}
