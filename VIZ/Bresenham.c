b59fe482552ea3738e028df04eb6e03eaec1531b
Aurametrix <irene@aurametrix.com>
2017-08-23 06:53:38 -0500

    simple example of Bresenham's Algorithm
    http://members.chello.at/~easyfilter/bresenham.html
    https://news.ycombinator.com/item?id=15074080
    http://free.pages.at/easyfilter/bresenham.html
    http://members.chello.at/~easyfilter/bresenham.c
    http://members.chello.at/~easyfilter/canvas.html

---
+++ VIZ/Bresenham.c
@@ -0,0 +1,15 @@
+void plotLine(int x0, int y0, int x1, int y1)
+{
+   int dx =  abs(x1-x0), sx = x0<x1 ? 1 : -1;
+   int dy = -abs(y1-y0), sy = y0<y1 ? 1 : -1; 
+   int err = dx+dy, e2; /* error value e_xy */
+ 
+   for(;;){  /* loop */
+      setPixel(x0,y0);
+      if (x0==x1 && y0==y1) break;
+      e2 = 2*err;
+      if (e2 >= dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
+      if (e2 <= dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
+   }
+}
+  
