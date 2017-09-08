d969fbf354f5d1fd7bbd46931569a0e3971ead64
Aurametrix <irene@aurametrix.com>
2017-09-07 18:55:21 -0700

    GCC to optimize isnan(x) || isnan(y) into isunordered(x, y)
    https://news.ycombinator.com/item?id=15187505
    Register allocation: http://compilers.cs.uni-saarland.de/projects/ssara/
    
    https://gcc.gnu.org/viewcvs/gcc?view=revision&revision=222077

--- TEST/missed-optimizations.c
+++ TEST/missed-optimizations.c
@@ -8,3 +8,12 @@ struct S0 {
 int f1(struct S0 p) {
     return p.f0;
 }
+
+
+
+PR tree-optimization/63387
+gcc/
+	* match.pd ((x unord x) | (y unord y) -> (x unord y),
+	(x unord x) | (x unord y) -> (x unord y)): New simplifications.
+gcc/testsuite/
+	* gcc.dg/pr63387.c: New testcase.
